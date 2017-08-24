eb95b7742408f54a449639398b8435d1528de6a4
schustda <douglas.schuster303@gmail.com>
2017-08-24 10:38:46 -0600

    aws

--- src/create_and_update_data.py
+++ src/create_and_update_data.py
@@ -7,9 +7,13 @@ import time
 if __name__ == '__main__':
 
     while True:
+
+
         df = pd.read_json('data/stock_list.json')
         for symbol in df.columns.tolist():
+            print (df[symbol]['url'])
             data = IhubData(df[symbol]['url'],verbose = 1)
+
             data.pull_posts()
             s = StockData(symbol)
             s.add_stock_data()
