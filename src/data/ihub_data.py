3a36248a21bfee5b5806a96dc85afab2bc2cd937
schustda <douglas.schuster303@gmail.com>
2017-08-31 17:57:00 -0600

    revised ihub script

--- src/data/ihub_data.py
+++ src/data/ihub_data.py
@@ -1,9 +1,9 @@
 import requests
 import pandas as pd
+import numpy as np
 import pathlib
 from bs4 import BeautifulSoup
 from time import time, sleep
-
 
 class IhubData(object):
 
@@ -15,36 +15,39 @@ class IhubData(object):
 
     def _total_and_num_pinned(self):
         '''
-        Parameters
-        ----------
-        None
-
         Output
         ------
-        num_pinned: int, shows how many 'pinned' posts there are on the specific
-            message board
+        num_pinned: int, shows how many posts are 'pinned' on the top of the
+            board.
         num_posts: int, shows, to-date, how many messages have been posted on
             the specific board
         '''
+        # Retrieve the first page on the board
         df, _ = self._get_page(self.url,most_recent=True,sort = False)
+
+        # Number of pinned posts determined by the number of posts that are not
+        # in 'numerical' order at the top of the page
         post_nums = df.index.tolist()
         for i in range(len(post_nums)):
             if post_nums[i] == post_nums[i+1]+1:
                 return i, post_nums[i]
 
-    def _clean_dataframe(self, df,sort):
+    def _clean_dataframe(self, df, sort):
         '''
         Parameters
         ----------
         df: pandas dataframe,
-        sort: boolean, as displayed on the webpage, the message board posts are
-            displayed in descending order. Sort sorts them
+        sort: boolean, the message board posts are displayed in descending
+            order. Sort=True sorts them
 
         Output
         ------
-        df: pandas dataframe, functions implemented: convert subject and
-            username to text, strip line breaks, convert 'date' to datetime
+        df: pandas dataframe, message board table
+
+        functions implemented: convert subject and username to text, strip line
+        breaks, convert 'date' to datetime
         '''
+
         df = df.applymap(lambda x: x.text)
         df[[0,1,2]] = df[[0,1,2]].applymap(lambda x: x.strip('-#\n\r').replace('\n', "").replace('\r',''))
         df[3] = pd.to_datetime(df[3]).dt.date
@@ -52,13 +55,14 @@ class IhubData(object):
         if sort:
             df.sort_values([0],inplace = True)
         df.set_index(0,inplace = True)
+
         return df
 
     def _get_page(self, post_number = 1, most_recent = False, sort = True, error_list = []):
         '''
         Parameters
         ----------
-        post_number: int, goes to a specific page if identified
+        post_number: int, specific post number of page to be returned
         most_recent: boolean, returns the currently displayed page if True
         sort: boolean, as displayed on the webpage, the message board posts are
             displayed in descending order. Sort sorts them
@@ -92,12 +96,56 @@ class IhubData(object):
             return pd.DataFrame(), error_list
 
     def _verbose(self, percent, original_time, time_elapsed):
+        '''
+        Method displays progress of retriving message board posts
+        '''
+
         a = int(percent/2)
         b = 50-a
         if percent == 0:
             percent = 0.5
-        min_remaining = int(time_elapsed/percent*(100-percent)/60)
-        print ('|'+ a*'=' + b*'-' + '| ' + str(percent) + '% - ' + str(min_remaining) + ' minute(s) remaining')
+        min_rem = int(time_elapsed/percent*(100-percent)/60)
+        print ('|{0}{1}| {2}% - {3} minute(s) remaining'.format(a*'=',b*'-',str(percent),str(min_rem)))
+
+    def _add_deleted_posts(self, df):
+        '''
+        Parameters
+        ----------
+        df: pandas dataframe, full message board data
+
+        Output
+        ------
+        df: pandas dataframe, original data with deleted posts added in
+
+        Moderators of a message board forum may remove a post if it violates
+        the ihub policy. While the content of these posts is unknown the actual
+        post is important when suming the posts per a given day.
+        '''
+
+        #get missing post numbers
+        deleted_post_set = set(range(1,df.index.max())).difference(set(df.index))
+
+        #create df with deleted posts
+        df_deleted = pd.DataFrame(np.nan, index=range(len(deleted_post_set)), columns=df.columns.tolist())
+        df_deleted.index = deleted_post_set
+        df_deleted.index.name = 'post_number'
+        df_deleted.subject = '<DELETED>'
+        df_deleted.username = '<DELETED>'
+
+        #add to original dataframe
+        df = pd.concat([df,df_deleted])
+
+        #sort df
+        df.sort_index(inplace=True)
+
+        # The dates from the deleted posts will be interpreted as from the
+        # day from the previous post
+        df.date.fillna(method = 'ffill',inplace = True)
+
+        # Some of the subjects are empty
+        df.subject.fillna('None',inplace = True)
+
+        return df
 
     def pull_posts(self):
         '''
@@ -110,9 +158,9 @@ class IhubData(object):
 
         # Determines whether or not the file has already been created
         # (need to create or update??)
-        path = pathlib.Path('data/raw_data/ihub/message_boards/'+self.symbol+'.csv')
+        path = pathlib.Path('data/raw_data/ihub/message_boards/'+self.symbol+'.csv').is_file()
 
-        if not path.is_file():
+        if not path:
 
             # if this is the first time this ticker symbol has been run
             print('pulling posts for ' + self.symbol)
@@ -149,6 +197,7 @@ class IhubData(object):
             df = pd.concat([df,page_df])
 
             if self.verbose:
+                # Display update ever 60 seconds
                 if time() > t + 60:
                     percent = int((post_num-start_page)/(end_page-start_page)*100)
                     time_elapsed = time() - original_time
@@ -169,8 +218,10 @@ class IhubData(object):
         df.index.name = 'post_number'
         df.drop_duplicates(inplace = True)
 
+        df = self._add_deleted_posts(df)
+
         # display whether or not the file was created or updated
-        if not path.is_file():
+        if not path:
             print ('complete, ' + str(self.num_posts) + ' posts created')
         else:
             print ('complete, ' + str(missing_posts) + ' post(s) added')
@@ -184,7 +235,7 @@ if __name__ == '__main__':
 
     kget = 'CaliPharms-Inc-KGET-10313'
     cbyi = 'Cal-Bay-International-Inc-CBYI-5520'
-    data = IhubData(cbyi,verbose = 1)
+    data = IhubData('cbyi',cbyi,verbose = 1)
     data.pull_posts()
 
 
